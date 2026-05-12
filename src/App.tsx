a655e45f1b1d0943012a146d2862170d2e2914c2
Quoc Bao Nguyen <3flowers2004@gmail.com>
2026-05-12 23:53:05 +0700

    add source

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import Chat from "./components/Chat.tsx"
+
+export default function App(){
+  return <Chat />
+}
---
+++ src/components/Chat.tsx
@@ -0,0 +1,33 @@
+import {useState} from "react"
+
+export default function Chat(){
+  // state for input 
+  const [input, setInput] = useState("")
+  //state for storing messages
+  const [messages, setMessages] = useState([])
+
+  // show the input log via f12 
+  console.log(input)
+
+  function sendMessages(){
+    setMessages([...messages, input]) // unpack the messages array and add input after these element
+    setInput("") // set the variable equal empty to clear the input field after send message
+  }
+
+  return (
+    <>
+      <input
+        value={input} // set value equal input (so we can clear the <input> by empty the input of state)
+        placeholder="Type something here!"
+        onChange={(e) => setInput(e.target.value)} // write the input to the variable of state (input variable)
+      />
+
+      <button onClick={sendMessages}> Send </button>
+      <button onClick={ () => setInput("") }> Clear </button>
+
+      <div>
+        {messages.map((msg, index) => <p key={index}>{msg}</p>)}
+      </div>
+    </>
+  )
+}
